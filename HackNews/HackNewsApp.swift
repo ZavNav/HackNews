bb089d92cee3d9c543ad6909d5317ccbe9bf5745
ZavNav <arka146@mail.ru>
2022-03-16 20:45:09 +0300

    Initial Commit

---
+++ HackNews/HackNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackNewsApp.swift
+//  HackNews
+//
+//  Created by Аркадий Рожков on 16.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct HackNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
